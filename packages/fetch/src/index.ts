b45c945d715073054f04e7a3cb5cc3ac50ae8984
Okiki Ojo <hey@okikio.dev>
2026-07-05 02:25:34 -0400

    feat(fetch): add deno.jsonc, module tsdoc, fix return types, & rename .js file imports to .ts
    Signed-off-by: Okiki Ojo <hey@okikio.dev>

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,4 +1,10 @@
-export { createFetch, type FetchReturn, type FetchOptions, type RequestContext } from "./fetch.js";
+/**
+ * Primitive that wraps fetch requests
+ *
+ * @module @solid-primitives/fetch
+ */
+
+export { createFetch, type FetchReturn, type FetchOptions, type RequestContext } from "./fetch.ts";
 export {
   withAbort,
   withAggregation,
@@ -8,6 +14,6 @@ export {
   withRefetchEvent,
   wrapFetcher,
   wrapResource,
-} from "./modifiers.js";
-export { withCache, withRefetchOnExpiry, withCacheStorage, serializeRequest } from "./cache.js";
-export { fetchRequest } from "./request.js";
+} from "./modifiers.ts";
+export { withCache, withRefetchOnExpiry, withCacheStorage, serializeRequest } from "./cache.ts";
+export { fetchRequest } from "./request.ts";
